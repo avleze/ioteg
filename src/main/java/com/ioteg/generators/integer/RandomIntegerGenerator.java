3774ce483a04c953757b75eeba568e423f9de15a
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-01-21 12:53:42 +0100

    Improving code quality of RandomIntegerGenerator #7 #9

--- src/main/java/com/ioteg/generators/integer/RandomIntegerGenerator.java
+++ src/main/java/com/ioteg/generators/integer/RandomIntegerGenerator.java
@@ -1,16 +1,25 @@
 package com.ioteg.generators.integer;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 import com.ioteg.model.Field;
 
 public class RandomIntegerGenerator implements IntegerGenerable {
+	
 
 	@Override
 	public Integer generate(Field integer) {
 		Integer min = integer.getMin().intValue();
 		Integer max = integer.getMax().intValue();
-		Random r = new Random();
-		Integer result = min + r.nextInt(max - min);
+		Integer result = null;
+		Random r;
+		try {
+			r = SecureRandom.getInstanceStrong();
+			result = min + r.nextInt(max - min);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}  
 
 		return result;
 	}
