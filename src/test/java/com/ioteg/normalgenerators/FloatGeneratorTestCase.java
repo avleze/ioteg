08e2e00b919a9f126d44db691568c6276369f2fc
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-02-18 21:34:29 +0100

    Decoupling com.ioteg.FloatGeneratorTestCase.java from EventGenerator.java.
    	modificado:     src/test/java/com/ioteg/normalgenerators/FloatGeneratorTestCase.java

--- src/test/java/com/ioteg/normalgenerators/FloatGeneratorTestCase.java
+++ src/test/java/com/ioteg/normalgenerators/FloatGeneratorTestCase.java
@@ -1,75 +1,90 @@
 package com.ioteg.normalgenerators;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import static org.junit.Assert.assertThat;
 import java.util.List;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultField;
+import com.ioteg.resultmodel.ResultSimpleField;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FloatGeneratorTestCase {
-	private static List<Element> fields;
 
-	@BeforeAll
-	public static void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		ClassLoader classLoader = FloatGeneratorTestCase.class.getClassLoader();
-		File xmlFile = new File(classLoader.getResource("./generators/testRandomFloatGenerator.xml").getFile());
-		Document document = builder.build(xmlFile);
-
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
-	}
+	private static final double DEFAULT_MAX = 10.0;
+	private static final double DEFAULT_MIN = 0.0;
 
 	@Test
-	public void testRandomWithDefaultRange() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testRandomWithDefaultRange() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setMin(DEFAULT_MIN);
+		field.setMax(DEFAULT_MAX);
+		field.setPrecision(2);
+		field.setName("test");
+		field.setQuotes(false);
+		field.setType("Float");
 
-		Element field = fields.get(0);
+		/* <field name="test" quotes="false" precision="2" type="Float"></field> */
 
-		for (int i = 0; i < 10000; ++i) {
-			String strResult = EventGenerator.generateValueSimpleType(field);
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+		List<ResultField> results = generator.generate(100);
+
+		for (ResultField rF : results) {
+			String strResult = ((ResultSimpleField) rF).getValue();
 			Double result = Double.parseDouble(strResult);
-			assertTrue(0.0 <= result);
-			assertTrue(result <= 10);
+			assertThat(result, greaterThanOrEqualTo(0.0));
+			assertThat(result, lessThanOrEqualTo(10.0));
 		}
 	}
 
 	@Test
-	public void testRandomWithSpecifiedRange() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testRandomWithSpecifiedRange() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setMin(23.54);
+		field.setMax(32.58);
+		field.setPrecision(2);
+		field.setName("test");
+		field.setQuotes(false);
+		field.setType("Float");
 
-		/** Test within a specified range **/
-		Element field = fields.get(1);
+		/*
+		 * <field name="test" quotes="false" type="Float" min="23.54"
+		 * max="32.58"></field>
+		 */
 
-		for (int i = 0; i < 10000; ++i) {
-			String strResult = EventGenerator.generateValueSimpleType(field);
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+		List<ResultField> results = generator.generate(100);
+
+		for (ResultField rF : results) {
+			String strResult = ((ResultSimpleField) rF).getValue();
 			Double result = Double.parseDouble(strResult);
-			assertTrue(23.54 <= result);
-			assertTrue(result <= 32.58);
+			assertThat(result, greaterThanOrEqualTo(23.54));
+			assertThat(result, lessThanOrEqualTo(32.58));
 		}
 
 	}
 
 	@Test
-	public void testFixedValue() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testFixedValue() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setValue("104.567");
+		field.setName("test");
+		field.setQuotes(false);
+		field.setType("Float");
 
-		/** Test of specified value **/
-		Element field = fields.get(2);
-		String strResult = EventGenerator.generateValueSimpleType(field);
+		/* <field name="test" quotes="false" type="Float" value="104.567"></field> */
+
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+		String strResult = ((ResultSimpleField) generator.generate(1).get(0)).getValue();
 		Double result = Double.parseDouble(strResult);
-		assertEquals(Double.valueOf("104.567"), result);
+		assertThat(result, equalTo(104.567));
 	}
 
 }
