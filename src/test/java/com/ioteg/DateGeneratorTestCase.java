d79381fee95f2e3bf1258d3c2dc513c4899f16ff
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-02-18 14:15:44 +0100

    Decoupling com.ioteg.DateGeneratorTestCase.java from EventGenerator.java.
    	modificado:     src/test/java/com/ioteg/DateGeneratorTestCase.java

--- src/test/java/com/ioteg/DateGeneratorTestCase.java
+++ src/test/java/com/ioteg/DateGeneratorTestCase.java
@@ -2,54 +2,58 @@ package com.ioteg;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultField;
+import com.ioteg.resultmodel.ResultSimpleField;
 
 public class DateGeneratorTestCase {
-	private static List<Element> fields;
-
-	@BeforeAll
-	public static void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		ClassLoader classLoader = DateGeneratorTestCase.class.getClassLoader();
-		File xmlFile = new File(classLoader.getResource("./generators/testDateGenerator.xml").getFile());
-		Document document = builder.build(xmlFile);
-
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
-	}
 
 	@Test
-	public void testRandom() throws JDOMException, IOException, ParseException, NotExistingGeneratorException, ExprLangParsingException {
-		Element field = fields.get(0);
+	public void testRandom() throws NotExistingGeneratorException, ExprLangParsingException, ParseException {
+		Field field = new Field();
+		field.setType("Date");
+		field.setQuotes(true);
+		field.setFormat("yy-MM-DD");
 
-		for (int i = 0; i < 1000; ++i) {
-			String strResult = EventGenerator.generateValueSimpleType(field);
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+
+		List<ResultField> results = generator.generate(100);
+
+		/* <field name="date" quotes="true" type="Date" format="yy-MM-DD"></field> */
+
+		for (ResultField rF : results) {
+			String strResult = ((ResultSimpleField) rF).getValue();
 			SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-DD");
 			sdf.parse(strResult);
 		}
 	}
 
 	@Test
-	public void testFixedValue() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
-		Element field = fields.get(1);
-		String strResult = EventGenerator.generateValueSimpleType(field);
+	public void testFixedValue() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setType("Date");
+		field.setQuotes(true);
+		field.setFormat("yy-mm-dd");
+		field.setValue("08-05-01");
+
+		/*
+		 * <field name="date" quotes="true" type="Date" value="08-05-01"
+		 * format="yy-mm-dd"></field>
+		 */
+
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+
+		String strResult = ((ResultSimpleField) generator.generate(1).get(0)).getValue();
 
 		assertTrue(strResult.equals("08-05-01"));
 	}
