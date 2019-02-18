507decca1ac3f69866aee9a55bb43bb72e7ac146
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-02-18 14:20:40 +0100

    Decoupling com.ioteg.TimeGeneratorTestCase.java from EventGenerator.java.
    	modificado:     src/test/java/com/ioteg/TimeGeneratorTestCase.java

--- src/test/java/com/ioteg/TimeGeneratorTestCase.java
+++ src/test/java/com/ioteg/TimeGeneratorTestCase.java
@@ -2,46 +2,37 @@ package com.ioteg;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultField;
+import com.ioteg.resultmodel.ResultSimpleField;
 
 public class TimeGeneratorTestCase {
-	private static List<Element> fields;
-
-	@BeforeAll
-	public static void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		ClassLoader classLoader = TimeGeneratorTestCase.class.getClassLoader();
-		File xmlFile = new File(classLoader.getResource("./generators/testTimeGenerator.xml").getFile());
-		Document document = builder.build(xmlFile);
-
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
-
-	}
 
 	@Test
-	public void testRandom() throws JDOMException, IOException, ParseException, NotExistingGeneratorException, ExprLangParsingException {
-		Element field = fields.get(0);
+	public void testRandom() throws NotExistingGeneratorException, ExprLangParsingException, ParseException {
+		Field field = new Field();
+		field.setType("Time");
+		field.setQuotes(true);
+		field.setFormat("hh:mm");
 
-		for (int i = 0; i < 1000; ++i) {
-			String strResult = EventGenerator.generateValueSimpleType(field);
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+
+		List<ResultField> results = generator.generate(100);
+
+		/* <field name="time" quotes="true" type="Time" format="hh:mm"></field> */
+
+		for (ResultField rF : results) {
+			String strResult = ((ResultSimpleField) rF).getValue();
 			SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
 			sdf.parse(strResult);
 		}
@@ -49,9 +40,21 @@ public class TimeGeneratorTestCase {
 	}
 
 	@Test
-	public void testFixedValue() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
-		Element field = fields.get(1);
-		String strResult = EventGenerator.generateValueSimpleType(field);
+	public void testFixedValue() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setType("Time");
+		field.setQuotes(true);
+		field.setFormat("HH:mm");
+		field.setValue("14:24");
+
+		/*
+		 * <field name="time" quotes="true" type="Time" value="14:24"
+		 * format="HH:mm"></field>
+		 */
+
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+
+		String strResult = ((ResultSimpleField) generator.generate(1).get(0)).getValue();
 		assertTrue(strResult.equals("14:24"));
 	}
 
