8dc33f5075982d63514591558d4f821be1d1c186
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-02-18 21:11:08 +0100

    Decoupling com.ioteg.LongGeneratorTestCase.java from EventGenerator.java.
    	modificado:     src/test/java/com/ioteg/normalgenerators/LongGeneratorTestCase.java

--- src/test/java/com/ioteg/normalgenerators/LongGeneratorTestCase.java
+++ src/test/java/com/ioteg/normalgenerators/LongGeneratorTestCase.java
@@ -1,71 +1,88 @@
 package com.ioteg.normalgenerators;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import static org.junit.Assert.assertThat;
 import java.util.List;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultField;
+import com.ioteg.resultmodel.ResultSimpleField;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.equalTo;
 
 public class LongGeneratorTestCase {
-	private static List<Element> fields;
-
-	@BeforeAll
-	public static void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		ClassLoader classLoader = LongGeneratorTestCase.class.getClassLoader();
-		File xmlFile = new File(classLoader.getResource("./generators/testLongGenerator.xml").getFile());
-		Document document = builder.build(xmlFile);
-
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
-	}
 
 	@Test
-	public void testRandomWithSpecifiedRange() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testRandomWithSpecifiedRange() throws NotExistingGeneratorException, ExprLangParsingException {
 
-		/** Test within a specified range **/
-		Element field = fields.get(0);
-		String strResult = EventGenerator.generateValueSimpleType(field);
+		Field field = new Field();
+		field.setMin(-10000000.0);
+		field.setMax(0.0);
+		field.setName("test");
+		field.setQuotes(false);
+		field.setType("Long");
+
+		/*
+		 * <field name="test" quotes="false" type="Long" min="-10000000"
+		 * max="0"></field>
+		 */
+
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+
+		String strResult = ((ResultSimpleField) generator.generate(1).get(0)).getValue();
+
 		Long result = Long.parseLong(strResult);
-		assertTrue(Long.valueOf("-10000000") < result);
-		assertTrue(result <= 0);
+		assertThat(result, greaterThan(Long.valueOf("-10000000")));
+		assertThat(result, lessThanOrEqualTo(Long.valueOf("0")));
 	}
 
 	@Test
-	public void testRandomWithDefaultRange() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testRandomWithDefaultRange() throws NotExistingGeneratorException, ExprLangParsingException {
+		Field field = new Field();
+		field.setMin(0.0);
+		field.setMax(10.0);
+		field.setName("testDefaultRange");
+		field.setQuotes(true);
+		field.setType("Long");
 
-		/** Test of default range **/
-		Element field = fields.get(1);
+		/*
+		 * 	<field name="testDefaultRange" quotes="true" type="Long"></field>
+		 */
 
-		for (int i = 0; i < 100; ++i) {
-			String strResult = EventGenerator.generateValueSimpleType(field);
-			Long result = Long.parseLong(strResult);
-			assertTrue(0 <= result);
-			assertTrue(result <= 9);
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+		List<ResultField> results = generator.generate(100);
+
+		for (ResultField rF : results) {
+			String strResult = ((ResultSimpleField) rF).getValue();
+			Long result = Long.valueOf(strResult);
+			assertThat(result, greaterThanOrEqualTo(0L));
+			assertThat(result, lessThanOrEqualTo(9L));
 		}
 	}
 
 	@Test
-	public void testFixedValue() throws JDOMException, IOException, NotExistingGeneratorException, ExprLangParsingException {
+	public void testFixedValue() throws NotExistingGeneratorException, ExprLangParsingException {
+		
+		Field field = new Field();
+		field.setMin(0.0);
+		field.setMax(10.0);
+		field.setName("testDefaultRange");
+		field.setQuotes(true);
+		field.setType("Long");
+		field.setValue("9223372036854775807");
 
-		/** Test of specified value **/
-		Element field = fields.get(2);
-		String strResult = EventGenerator.generateValueSimpleType(field);
+		/*<field name="testDefaultValue" quotes="false" type="Long" value="9223372036854775807"></field>*/
+
+		Generable generator = GeneratorsFactory.makeGenerator(field, null);
+		String strResult = ((ResultSimpleField) generator.generate(1).get(0)).getValue();
 		Long result = Long.parseLong(strResult);
-		assertEquals(Long.valueOf("9223372036854775807"), result);
+		assertThat(result, equalTo(9223372036854775807L));
 	}
 }
