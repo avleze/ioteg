65b65def0787e23a20ffe78a74024f6bfba66fd4
Antonio Vélez Estévez <antonio.velezestevez@alum.uca.es>
2019-02-21 13:59:52 +0100

    Decoupling com.ioteg.StringQueryRestrictionTestCase.java from EventGenerator.java
    	modificado:     src/test/java/com/ioteg/TimeGeneratorQueryRestrictionTestCase.java

--- src/test/java/com/ioteg/StringGeneratorQueryRestrictionTestCase.java
+++ src/test/java/com/ioteg/StringGeneratorQueryRestrictionTestCase.java
@@ -1,147 +1,153 @@
 package com.ioteg;
 
 import static org.junit.Assert.assertThat;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultSimpleField;
 
 import static org.hamcrest.Matchers.matchesPattern;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
-
-
 public class StringGeneratorQueryRestrictionTestCase {
 
-	private static File xmlFile;
-	private static ClassLoader classLoader;
-	private static List<Element> fields;
+	private static final int DEFAULT_LENGTH = 10;
 
-	@BeforeEach
-	public void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		classLoader = StringGeneratorQueryRestrictionTestCase.class.getClassLoader();
-		xmlFile = new File(classLoader.getResource("./EPLSamples/testEplQuery.xml").getFile());
-		Document document = builder.build(xmlFile);
+	@Test
+	public void testStringQueryRestrictionEqualOperator() throws NotExistingGeneratorException, ExprLangParsingException
+	{
+		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field23");
+		field.setLength(DEFAULT_LENGTH);
 
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field23", "=", "HOLA ESTO ES UNA PRUEBA"));
+
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+
+		assertThat(rF.getValue(), equalTo("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue().length(), equalTo(23));
 	}
 
 	@Test
-	public void testStringQueryRestrictionEqualOperator() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(22);
+	public void testStringQueryRestrictionNotEqualOperator() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field23");
+		field.setLength(DEFAULT_LENGTH);
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, equalTo("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result.length(), equalTo(23));
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field23", "!=", "HOLA ESTO ES UNA PRUEBA"));
+
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+
+		assertThat(rF.getValue(), not(equalTo("HOLA ESTO ES UNA PRUEBA")));
+		assertThat(rF.getValue().length(), equalTo(10));
 	}
-	
+
 	@Test
-	public void testStringQueryRestrictionNotEqualOperator() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(22);
+	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacter() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field24");
+		field.setEndcharacter("F");
+		field.setLength(DEFAULT_LENGTH);
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result.length(), equalTo(10));
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field24", "!=", "HOLA ESTO ES UNA PRUEBA"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+		assertThat(rF.getValue(), not("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue(), matchesPattern("[ABCDEF]*"));
+		assertThat(rF.getValue().length(), equalTo(10));
 	}
-	
+
 	@Test
-	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacter() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(23);
+	public void testStringQueryRestrictionNotEqualOperatorWithLength() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field25");
+		field.setLength(12);
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field25", "!=", "HOLA ESTO ES UNA PRUEBA"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result, matchesPattern("[ABCDEF]*"));
-		assertThat(result.length(), equalTo(10));
+		assertThat(rF.getValue(), not("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue().length(), equalTo(12));
 	}
-	
+
 	@Test
-	public void testStringQueryRestrictionNotEqualOperatorWithLength() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(24);
-
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result.length(), equalTo(12));
+	public void testStringQueryRestrictionNotEqualOperatorWithLengthAndCase() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field26");
+		field.setLength(12);
+		field.setCase("low");
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field26", "!=", "HOLA ESTO ES UNA PRUEBA"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+		assertThat(rF.getValue(), not("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue().length(), equalTo(12));
+		assertThat(rF.getValue(), equalTo(rF.getValue().toLowerCase()));
 	}
-	
+
 	@Test
-	public void testStringQueryRestrictionNotEqualOperatorWithLengthAndCase() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(26);
+	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacterAndCase() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field27");
+		field.setCase("low");
+		field.setEndcharacter("F");
+		field.setLength(DEFAULT_LENGTH);
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field27", "!=", "HOLA ESTO ES UNA PRUEBA"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result.length(), equalTo(12));
-		assertThat(result, equalTo(result.toLowerCase()));
+		assertThat(rF.getValue(), not("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue(), equalTo(rF.getValue().toLowerCase()));
+		assertThat(rF.getValue(), matchesPattern("[abcdef]*"));
 	}
-	
+
 	@Test
-	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacterAndCase() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(27);
+	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacterAndLength() throws NotExistingGeneratorException, ExprLangParsingException
+	{		Field field = new Field();
+		field.setType("String");
+		field.setQuotes(false);
+		field.setName("field25");
+		field.setEndcharacter("F");
+		field.setLength(12);
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result, equalTo(result.toLowerCase()));
-		assertThat(result, matchesPattern("[abcdef]*"));
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field25", "!=", "HOLA ESTO ES UNA PRUEBA"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+
+		assertThat(rF.getValue(), not("HOLA ESTO ES UNA PRUEBA"));
+		assertThat(rF.getValue(), matchesPattern("[ABCDEF]*"));
+		assertThat(rF.getValue().length(), equalTo(12));
 	}
-	
-	@Test
-	public void testStringQueryRestrictionNotEqualOperatorWithEndCharacterAndLength() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/StringOperatorExamples/EPLStringQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(25);
-
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("HOLA ESTO ES UNA PRUEBA"));
-		assertThat(result, matchesPattern("[ABCDEF]*"));
-		assertThat(result.length(), equalTo(12));
-	}
-
 
 }
--- src/test/java/com/ioteg/TimeGeneratorQueryRestrictionTestCase.java
+++ src/test/java/com/ioteg/TimeGeneratorQueryRestrictionTestCase.java
@@ -2,69 +2,55 @@ package com.ioteg;
 
 import static org.junit.Assert.assertThat;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.ioteg.EventGenerator;
 import com.ioteg.exprlang.ExprParser.ExprLangParsingException;
+import com.ioteg.generators.Generable;
+import com.ioteg.generators.GeneratorsFactory;
 import com.ioteg.generators.exceptions.NotExistingGeneratorException;
+import com.ioteg.model.Field;
+import com.ioteg.resultmodel.ResultSimpleField;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
-
-
 public class TimeGeneratorQueryRestrictionTestCase {
 
-	private static File xmlFile;
-	private static ClassLoader classLoader;
-	private static List<Element> fields;
+	private static Field field;
 
-	@BeforeEach
-	public void loadSchema() throws JDOMException, IOException {
-		SAXBuilder builder = new SAXBuilder();
-		classLoader = TimeGeneratorQueryRestrictionTestCase.class.getClassLoader();
-		xmlFile = new File(classLoader.getResource("./EPLSamples/testEplQuery.xml").getFile());
-		Document document = builder.build(xmlFile);
-
-		List<Element> blocks = document.getRootElement().getChildren("block");
-		fields = blocks.get(0).getChildren("field");
-		EventGenerator.fieldvalues = new ArrayList<>();
+	@BeforeAll
+	public static void initialize() {
+		field = new Field();
+		field.setType("Time");
+		field.setName("field36");
+		field.setFormat("HH:mm");
 	}
 
 	@Test
-	public void testTimeQueryRestrictionEqualOperator() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/TimeOperatorExamples/EPLTimeQueryEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(35);
+	public void testTimeQueryRestrictionEqualOperator() throws NotExistingGeneratorException, ExprLangParsingException {
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field36", "=", "12:43"));
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, equalTo("12:43"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+
+		assertThat(rF.getValue(), equalTo("12:43"));
 	}
-	
+
 	@Test
-	public void testTimeQueryRestrictionNotEqualOperator() throws IOException, JDOMException, NotExistingGeneratorException, ExprLangParsingException {
-		SAXBuilder builder = new SAXBuilder();
-		Document document = builder.build(xmlFile);
-		EventGenerator.getEPLValues(classLoader.getResource("./EPLSamples/TimeOperatorExamples/EPLTimeQueryNotEqualOperator.epl").getPath(),
-				document.getRootElement());
-		
-		Element field = fields.get(35);
+	public void testTimeQueryRestrictionNotEqualOperator() throws NotExistingGeneratorException, ExprLangParsingException {
+		List<Trio<String, String, String>> restrictions = new ArrayList<>();
+		restrictions.add(new Trio<>("field36", "!=", "12:43"));
 
-		String result = EventGenerator.generateValueSimpleType(field);
-		assertThat(result, not("12:43"));
+		Generable generator = GeneratorsFactory.makeQueryRestrictionGenerator(field, restrictions);
+
+		ResultSimpleField rF = (ResultSimpleField) generator.generate(1).get(0);
+
+		assertThat(rF.getValue(), not(equalTo("12:43")));
 	}
-
 
 }
